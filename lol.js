e3a312ff6500e5b41f683eb8d2ed5044b684d5b0
Vivian Qian <l.valeria.lupa@gmail.com>
2017-06-30 18:18:11 +0000

    SharFood complete

---
+++ lol.js
@@ -0,0 +1,28 @@
+var ans;
+function lol() {
+   alert("$170.99")
+function hi() {
+    alert("$380.96")
+}
+function lel() {
+    alert("$21,290.93 ")
+}
+function heh() {
+    alert("$21,987.92")
+}
+function uhh(){
+    alert("$23,684.91")
+}
+function wel(){
+    alert("$23,989.86")
+}
+function umm(){
+    alert("$67,700,756.80")
+}
+function yay(){
+    alert("$67,700,756.80+ (depending)")
+}
+
+! function () {
+}
+
